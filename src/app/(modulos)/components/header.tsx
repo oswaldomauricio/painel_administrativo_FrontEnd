94ac23728894e95e00651ba868efee959f8d9633
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-11 12:15:27 -0400

    refactor: adicionando o perfil dentro do header

--- src/app/(modulos)/components/header.tsx
+++ src/app/(modulos)/components/header.tsx
@@ -1,9 +1,18 @@
 
 import { Layout } from "antd";
+import { ReactNode } from "react";
+
+type HeaderComponentProps = {
+  children: ReactNode;
+};
 
 const { Header } = Layout;
 
 
-export default function HeaderComponent() {
-  return <Header style={{ padding: 0}} />;
+export default function HeaderComponent({ children }: HeaderComponentProps) {
+  return (
+    <Header style={{ display: "flex", justifyContent: 'end', color: "white", background: 'white' }}>
+        {children}
+    </Header>
+  );
 }
