37b0c735376097b813c9d54797762e005cbe6329
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-15 13:58:38 -0400

    style: sem mudanças, apenas ajustando a identação

--- src/app/(modulos)/layout.tsx
+++ src/app/(modulos)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import "@ant-design/v5-patch-for-react-19";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/sidebar/sidebar";
 
 export async function generateMetadata() {
   return {
@@ -11,7 +11,9 @@ export async function generateMetadata() {
 export default function modulosLayout({ children }: { children: ReactNode }) {
   return (
     <div className="">
-      <Sidebar>{children}</Sidebar>
+      <Sidebar>
+        {children}
+      </Sidebar>
     </div>
   );
 }
