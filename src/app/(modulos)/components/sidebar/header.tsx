243f98e3597304819432f6753b855d4202644db7
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-15 14:03:24 -0400

    refactor: refatorando a sidebar e o header

---
+++ src/app/(modulos)/components/sidebar/header.tsx
@@ -0,0 +1,18 @@
+
+import { Layout } from "antd";
+import { ReactNode } from "react";
+
+type HeaderComponentProps = {
+  children: ReactNode;
+};
+
+const { Header } = Layout;
+
+
+export default function HeaderComponent({ children }: HeaderComponentProps) {
+  return (
+    <Header style={{ display: "flex", justifyContent: 'end', color: "white", background: 'white' }}>
+        <div>{children}</div>
+    </Header>
+  );
+}
---
+++ src/app/(modulos)/components/sidebar/sidebar.tsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { ReactNode, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { UploadOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+import HeaderComponent from "./header";
+import { useSession } from "next-auth/react";
+import {LogoutBtnProfile} from "../logoutBtn";
+
+const { Content, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    key: "financeiro",
+    label: "Financeiro",
+    icon: <UploadOutlined />,
+    children: [
+      {
+        key: "caixa",
+        label: "Caixa",
+      },
+      {
+        key: "caixa2",
+        label: "Caixa 2",
+      },
+    ],
+  },
+];
+
+export default function Sidebar({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const [current, setCurrent] = useState("financeiro");
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const { data: session, status } = useSession();
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrent(e.key);
+
+    const routes: Record<string, string> = {
+      caixa: "/financeiro/caixa",
+      caixa2: "/financeiro",
+    };
+
+    if (routes[e.key]) {
+      router.push(routes[e.key]);
+    }
+  };
+
+  return (
+    <Layout className="h-svh overflow-hidden">
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="flex items-center justify-center">
+          <Image
+            alt="Logo da empresa"
+            src="/logoBranca.png"
+            className="py-4"
+            priority={false}
+            width={100}
+            height={40}
+            quality={100}
+          />
+        </div>
+        <Menu
+          theme="dark"
+          onClick={onClick}
+          style={{ width: 200, color: "white" }}
+          selectedKeys={[current]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <HeaderComponent>
+          <div>
+            {status === "loading" && <div className="bg-zinc-50/10 animate-pulse rounded-md w-8 h-1 p-2">Carregando...</div>}
+            {session ? (
+              <div className="flex flex-row gap-2 items-center justify-center">
+                <div className="font-semibold text-black">
+                  {session.user?.name.toUpperCase() || "Usuário"}
+                </div>
+                <div className="w-[1px] h-5 bg-black" />
+                <Image
+                  src="/profileIcon.png"
+                  alt="perfil"
+                  width={40}
+                  height={20}
+                  quality={100}
+                />
+                <div className="w-[1px] h-5 bg-black" />
+                <LogoutBtnProfile />
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+        </HeaderComponent>
+        <Content style={{ margin: "16px 16px 0", overflow: "auto" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            {children}
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
