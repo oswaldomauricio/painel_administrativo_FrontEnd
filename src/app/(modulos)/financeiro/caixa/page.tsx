4dc573261e864ce11348355e08bb63431e2e5405
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-15 14:04:04 -0400

    refactor: adicionando o component de envio de valores.

--- src/app/(modulos)/financeiro/caixa/page.tsx
+++ src/app/(modulos)/financeiro/caixa/page.tsx
@@ -1,27 +1,28 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import LogoutForm from "@/app/(login)/components/logoutForm";
+// import LogoutForm from "@/app/(login)/components/logoutForm";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-
+// import Campo_envio_valores from "./campo_envio_valores";
+import Enviar_valores from "./enviar_valores";
 
 export default async function ModuloCaixa() {
+  
   const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/");
   }
-  
 
   return (
-    <div className="">
-      <h1>ola</h1>
-      <div>{session.user?.name}</div>
+    <div className="flex justify-center">
+      {/* <div>{session.user?.name}</div>
       <div>{session.user?.id}</div>
-      <div>{session.user?.role}</div>
-      <div>
+      <div>{session.user?.role}</div> */}
+      {/* <Campo_envio_valores /> */}
+      <Enviar_valores />
+      {/* <div>
         <LogoutForm />
-      </div>
+      </div> */}
     </div>
   );
 }
