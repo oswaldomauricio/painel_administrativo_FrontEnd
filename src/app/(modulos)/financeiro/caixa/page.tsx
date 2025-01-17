0336ce20c6845086f42f1b51e998cbbe2bea0fca
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-17 08:48:29 -0400

    refactor: alterando o estilo da modulo de caixa principal

--- src/app/(modulos)/financeiro/caixa/page.tsx
+++ src/app/(modulos)/financeiro/caixa/page.tsx
@@ -1,12 +1,10 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-// import LogoutForm from "@/app/(login)/components/logoutForm";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-// import Campo_envio_valores from "./campo_envio_valores";
 import Enviar_valores from "./enviar_valores";
-
+import { Col, Row } from "antd";
+import Relatorio_valores from "./relatorio_valores";
 export default async function ModuloCaixa() {
-  
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -14,15 +12,21 @@ export default async function ModuloCaixa() {
   }
 
   return (
-    <div className="flex justify-center">
-      {/* <div>{session.user?.name}</div>
-      <div>{session.user?.id}</div>
-      <div>{session.user?.role}</div> */}
-      {/* <Campo_envio_valores /> */}
-      <Enviar_valores />
-      {/* <div>
-        <LogoutForm />
-      </div> */}
+    <div className="">
+      <Row>
+        <Col span={24}>
+          <div className="flex justify-start">
+            <Enviar_valores />
+          </div>
+        </Col>
+      </Row>
+      <Row className="gap-6 justify-between py-8">
+        <Col span={24}>
+          <div className="">
+            <Relatorio_valores />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
