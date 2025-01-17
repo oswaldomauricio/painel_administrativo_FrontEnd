ac9931f90ff1f6df8cba97cad26c4c03023d639d
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-17 08:44:57 -0400

    feat: criando o componente de card e recebendo os valores de entrada, saida e saldo.

---
+++ src/app/(modulos)/components/cards/card_values_caixa.tsx
@@ -0,0 +1,32 @@
+import { type ComponentType } from 'react';
+
+interface CardValuesProps {
+  title: string;
+  value: number;
+  icon: ComponentType<{ className?: string }>; 
+  color: string; 
+}
+
+
+export function Card_values_caixa({ title, value, icon: Icon, color }: CardValuesProps) {
+  return (
+    <div className="bg-sidebar_main rounded-[35px] p-8 w-[300px] shadow-lg">
+      <div className="flex flex-row gap-4 items-center">
+        <div className="bg-sidebar_main rounded-lg p-3 w-fit shadow-xl">
+          <Icon className="text-white text-3xl" />
+        </div>
+        <div className={`${color}`}>
+          <p className="text-2xl mb-2">{title}</p>
+          <p className="text-4xl font-semibold">
+            <span className="text-3xl">
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+              }).format(value)}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
